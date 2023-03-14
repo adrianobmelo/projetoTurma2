1bf6d7162645cf6703c7fcb9b7971cf5c1f0757d
PedroHenriqueLeal <pedro.15.pl.pl@gmail.com>
2023-03-14 10:24:46 -0300

    Criação do Repositório

---
+++ projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/repository/ContaRepo.java
@@ -0,0 +1,9 @@
+package br.gama.itau.projetogrupo2.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.gama.itau.projetogrupo2.model.Conta;
+
+public interface ContaRepo extends CrudRepository<Conta, Long>{
+    
+}
