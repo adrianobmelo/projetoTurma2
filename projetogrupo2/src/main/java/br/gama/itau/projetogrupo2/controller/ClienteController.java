6155c960cbbcd8c6370fee1d9a7a5e516f050afb
deividydantas <deividy_dantasv@hotmail.com>
2023-03-14 15:38:03 -0300

    implementaçao controller e service do cliente

--- projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/controller/ClienteController.java
+++ projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/controller/ClienteController.java
@@ -3,8 +3,12 @@ package br.gama.itau.projetogrupo2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +22,29 @@ public class ClienteController {
     @Autowired
     private ClienteService service;
 
+    // pegar todos
     @GetMapping
     public ResponseEntity<List<Cliente>> getAll() {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    //pegar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+
+        Cliente cliente = service.getById(id);
+        return ResponseEntity.ok(cliente);
+    }
+
+    //criar
+    @PostMapping
+    public ResponseEntity<Cliente> newCliente(@RequestBody Cliente novoCliente) {
+        Cliente clienteInserido = service.newCliente(novoCliente);
+
+        if(clienteInserido == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteInserido);
     }
 }
--- projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/service/ClienteService.java
+++ projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/service/ClienteService.java
@@ -1,6 +1,8 @@
 package br.gama.itau.projetogrupo2.service;
 
 import java.util.List;
+import java.util.Optional;
+//import br.gama.itau.projetogrupo2.exception.NotFoundException;
 
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
+    // mostrar todos
     private final ClienteRepo repo;
 
     public List<Cliente> getAll() {
         return (List<Cliente>) repo.findAll();
+    }
+
+    // mostrar por id
+    public Cliente getById(long id) {
+        Optional<Cliente> clienteOptional = repo.findById(id);
+
+        if (clienteOptional.isEmpty()) {
+            //throw new NotFoundException("Cliente não encontrado");
+            System.out.println("Cliente não encontrado");
+        }
+
+        Cliente clienteEncontrado = clienteOptional.get();
+        return clienteEncontrado;
+    }
+
+    // add cliente
+    public Cliente newCliente(Cliente novoCliente) {
+        if(novoCliente.getIdCliente() > 0) {
+            return null;
+        }
+        Cliente clienteInserido = repo.save(novoCliente);
+        return clienteInserido;
     }
 }
