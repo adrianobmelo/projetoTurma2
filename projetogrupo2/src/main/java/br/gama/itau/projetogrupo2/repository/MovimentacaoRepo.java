d5b60c14da1cdb04bed44d7e2b62bb983a81c700
Flávio Machado <122133613+Flavio-GM@users.noreply.github.com>
2023-03-17 15:23:48 -0300

    Delete projetogrupo2 directory

--- projetogrupo2/src/main/java/br/gama/itau/projetogrupo2/repository/MovimentacaoRepo.java
+++
@@ -1,9 +0,0 @@
-package br.gama.itau.projetogrupo2.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.gama.itau.projetogrupo2.model.Movimentacao;
-
-public interface MovimentacaoRepo extends CrudRepository<Movimentacao ,Long> {
-    
-}
